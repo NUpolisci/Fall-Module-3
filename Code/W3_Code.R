36ae65906e95a90cc7bc63c50be8c79b09b4a626
lin-jennifer <jennifer.lin16@ncf.edu>
2021-11-20 16:31:07 -0600

    Week 3 materials

---
+++ Code/W3_Code.R
@@ -0,0 +1,64 @@
+# *****************************************************************
+# OVERVIEW ####
+# *****************************************************************
+# W3_Code.R
+# R Workshop: PS 490 Module 3 Week 3
+# Themes, Fonts... Ultimate SPICE UP!
+# Data: ANES 2020
+# ANES Data: https://electionstudies.org/data-center/
+# NAME: 
+# DATE: 2021 11 22
+
+# *****************************************************************
+# PACKAGES AND FUNCTIONS ####
+# *****************************************************************
+
+library(dplyr)      # For %>%, filter(), group_by(), summarize()
+library(ggplot2)    # For ggplot() and subquent functions
+
+# *****************************************************************
+# LOAD DATA ####
+# *****************************************************************
+
+# For the purpose of this course, I am providing a shortened, 
+# relatively more cleaned up version of the ANES. Therefore
+# this does not resemble what you may get if you download 
+# direcly from the ANES website. I am happy to shre the cleaning
+# code if needed but for the purposes of the goals of this
+# module, I did not want you to be bogged down by the mechanics
+# of the cleaning process. 
+
+# Put the data and this code in the SAME folder in your 
+# desktop. Open this script from that folder.
+# If that does not work, you many need to change the file 
+# path to suit your needs
+
+ANES <- read.csv("anes2020.csv")
+
+# *****************************************************************
+# BUILD GRAPH ####
+# *****************************************************************
+
+# Copy your final product from last week
+
+# Exercise 1 ------
+
+# In the code, do the following on the same chunk
+# - Add a global theme
+# - Add specific theme options
+
+
+# Exercise 2 ------
+
+# What is something that you like to enhance about you graph?
+
+
+
+# Stack Overflow link
+
+
+
+# Stack Overflow solution as applied to your graph
+
+
+
